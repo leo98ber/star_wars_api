281285f587ac0c2674cf556bab1dce6f2ed7a7a2
leo98ber <leonardoberoes94@gmail.com>
2022-11-01 21:04:55 -0400

    refactor: change name api folder

--- star_wars_caracters_api/urls.py
+++ star_wars_caracters_api/urls.py
@@ -1,4 +1,4 @@
-"""star_wars_caracter_api URL Configuration
+"""star_wars_caracters_api URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
--- star_wars_caracters_api/wsgi.py
+++ star_wars_caracters_api/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for star_wars_caracter_api project.
+WSGI config for star_wars_caracters_api project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'star_wars_caracter_api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'star_wars_caracters_api.settings')
 
 application = get_wsgi_application()
