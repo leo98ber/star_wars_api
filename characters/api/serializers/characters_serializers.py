7fcf57e34cd2bc00ec299b9daebbc1892d34a9d5
leo98ber <leonardoberoes94@gmail.com>
2022-11-02 23:28:17 -0400

    feat: add new serializer to list data with join from other table

--- characters/api/serializers/characters_serializers.py
+++ characters/api/serializers/characters_serializers.py
@@ -1,13 +1,19 @@
 from rest_framework import serializers
 
 from characters.models import Character
-from movies.api.serializers.movies_serializers import FilmSerializer
+from movies.api.serializers.movies_serializers import FilmSerializer, FilmJoinSerializer
 from movies.models import Film
 
 
-class CharacterSerializer(serializers.ModelSerializer):
-    movies = FilmSerializer(Film, many=True)
+class ListCharacterSerializer(serializers.ModelSerializer):
+    movies = FilmJoinSerializer(Film, many=True, read_only=True)
 
+    class Meta:
+        model = Character
+        fields = '__all__'
+
+
+class CharacterSerializer(serializers.ModelSerializer):
     class Meta:
         model = Character
         fields = '__all__'
