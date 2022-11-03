a8edb1f8c87d69ad0e02641d761b32f2c63eaff1
leo98ber <leonardoberoes94@gmail.com>
2022-11-02 20:24:10 -0400

    refactor: remove unnecessary TODO

--- base/models.py
+++ base/models.py
@@ -4,7 +4,6 @@ from django.db import models
 class BaseModel(models.Model):
     """Model definition for BaseModel."""
 
-    # TODO: Define fields here
     id = models.AutoField(primary_key=True)
     state = models.BooleanField('Estado', default=True)
     created_date = models.DateField('Fecha de Creación', auto_now=False, auto_now_add=True)
