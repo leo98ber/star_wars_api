0888300bbc16fd3cebd0d6e459ce34d192cb45d7
leo98ber <leonardoberoes94@gmail.com>
2022-11-02 23:30:35 -0400

    feat: add new route to filter data by list

--- characters/api/routers.py
+++ characters/api/routers.py
@@ -1,8 +1,9 @@
 from rest_framework.routers import DefaultRouter
 
-from characters.api.viewsets.characters_views import CharacterViewSet
+from characters.api.viewsets.characters_views import CharacterViewSet, FilterCharacterViewSet
 
 router = DefaultRouter()
 
 router.register(r'characters', CharacterViewSet, basename='characters')
+router.register(r'filter_characters', FilterCharacterViewSet, basename='filter_characters')
 urlpatterns = router.urls
