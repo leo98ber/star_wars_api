23f2c93c79969b1dd838f8d0f15592780452fdcd
leo98ber <leonardoberoes94@gmail.com>
2022-11-03 19:47:12 -0400

    refactor: change urls, remove unnecessary route

--- characters/api/routers.py
+++ characters/api/routers.py
@@ -1,9 +1,9 @@
 from rest_framework.routers import DefaultRouter
 
-from characters.api.viewsets.characters_views import CharacterViewSet, FilterCharacterViewSet
+from characters.api.viewsets.characters_views import CharacterViewSet
 
 router = DefaultRouter()
 
 router.register(r'characters', CharacterViewSet, basename='characters')
-router.register(r'filter_characters', FilterCharacterViewSet, basename='filter_characters')
+
 urlpatterns = router.urls
