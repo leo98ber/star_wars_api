39d8a82718e2853a12ac6fed6c0beed42ff000bd
leo98ber <leonardoberoes94@gmail.com>
2022-11-02 20:00:17 -0400

    feat!: add base model

--- base/models.py
+++ base/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+from simple_history.models import HistoricalRecords
+
+
 # Create your models here.
+class BaseModel(models.Model):
+    """Model definition for BaseModel."""
+
+    # TODO: Define fields here
+    id = models.AutoField(primary_key=True)
+    state = models.BooleanField('Estado', default=True)
+    created_date = models.DateField('Fecha de Creación', auto_now=False, auto_now_add=True)
+    modified_date = models.DateField('Fecha de Modificación', auto_now=True, auto_now_add=False)
+    deleted_date = models.DateField('Fecha de Eliminación', auto_now=True, auto_now_add=False)
+
+    class Meta:
+        """Meta definition for BaseModel."""
+        abstract = True  # No tiene una __str__
+        verbose_name = 'Modelo Base'
+        verbose_name_plural = 'Modelos Base'
