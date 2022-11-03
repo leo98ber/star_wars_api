e301e6894c8985f4c1f956e32532b49008d53bfd
leo98ber <leonardoberoes94@gmail.com>
2022-11-02 21:19:17 -0400

    feat: remove unnecessary fields in Base model

--- base/models.py
+++ base/models.py
@@ -7,8 +7,6 @@ class BaseModel(models.Model):
     id = models.AutoField(primary_key=True)
     state = models.BooleanField('Estado', default=True)
     created_date = models.DateField('Fecha de Creación', auto_now=False, auto_now_add=True)
-    modified_date = models.DateField('Fecha de Modificación', auto_now=True, auto_now_add=False)
-    deleted_date = models.DateField('Fecha de Eliminación', auto_now=True, auto_now_add=False)
 
     class Meta:
         """Meta definition for BaseModel."""
