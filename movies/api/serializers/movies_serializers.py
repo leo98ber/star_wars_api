e715719f09b3c264f692e3debe86c744631c412e
leo98ber <leonardoberoes94@gmail.com>
2022-11-02 23:28:53 -0400

    feat: add new serializer to get spesific data

--- movies/api/serializers/movies_serializers.py
+++ movies/api/serializers/movies_serializers.py
@@ -4,12 +4,12 @@ from movies.models import Film
 
 
 class FilmSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Film
         fields = '__all__'
 
-    def to_representation(self, instance):
-        return {
-            'name': instance.name
-        }
+
+class FilmJoinSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Film
+        fields = ['name']
