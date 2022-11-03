983e82267a4945cfbad4b5948e0d4e7f0002318a
leo98ber <leonardoberoes94@gmail.com>
2022-11-02 21:18:57 -0400

    feat: add character model

--- characters/models.py
+++ characters/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+from base.models import BaseModel
+from movies.models import Film
+
+
+class Character(BaseModel):
+    """Model definition for Character."""
+
+    name = models.CharField(max_length=150, unique=True, blank=False, null=False)
+    performer = models.CharField(max_length=150, blank=False, null=False)
+    movies = models.ManyToManyField(Film)
