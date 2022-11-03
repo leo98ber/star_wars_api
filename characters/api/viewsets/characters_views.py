f478941a0ecd9e5317731b8b4500f1ad513aadcf
leo98ber <leonardoberoes94@gmail.com>
2022-11-02 23:27:27 -0400

    feat: add FilterCharacterViewSet to make a query by parameters

--- characters/api/viewsets/characters_views.py
+++ characters/api/viewsets/characters_views.py
@@ -1,9 +1,22 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status, generics
+from rest_framework import viewsets, filters
 
-from base.views import GeneralListApiView
-from characters.api.serializers.characters_serializers import CharacterSerializer
+from base.views import GeneralListApiView, GeneralViewSet
+from characters.api.serializers.characters_serializers import CharacterSerializer, ListCharacterSerializer
 from characters.models import Character
+from rest_framework.response import Response
+
+from movies.api.serializers.movies_serializers import FilmSerializer
 
 
-class CharacterViewSet(GeneralListApiView):
+class CharacterViewSet(GeneralViewSet):
     serializer_class = CharacterSerializer
+
+
+class FilterCharacterViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = ListCharacterSerializer
+    queryset = serializer_class.Meta.model.objects.all()
+
+    filter_backends = [filters.SearchFilter]
+
+    search_fields = ['name', 'performer']
