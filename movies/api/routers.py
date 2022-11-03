e78e909b9fd57e81b263981f8e75b9463be97e64
leo98ber <leonardoberoes94@gmail.com>
2022-11-03 01:08:17 -0400

    feat: add planets into movies urls

--- movies/api/routers.py
+++ movies/api/routers.py
@@ -1,8 +1,11 @@
 from rest_framework.routers import DefaultRouter
 
 from movies.api.viewsets.movies_views import FilmViewSet
+from movies.api.viewsets.planets_views import PlanetViewSet
 
 router = DefaultRouter()
 
 router.register(r'movies', FilmViewSet, basename='movies')
+router.register(r'planets', PlanetViewSet, basename='planets')
+
 urlpatterns = router.urls
