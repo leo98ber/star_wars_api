46fa60cfa69ccc6c2ff5bba03fa7827fdebe774e
leo98ber <leonardoberoes94@gmail.com>
2022-11-02 20:25:45 -0400

    feat: add Film model to manipulate movies data

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+from base.models import BaseModel
+
+
+class Film(BaseModel):
+    """Model definition for Movie."""
+
+    name = models.CharField(max_length=150, unique=True, blank=False, null=False)
