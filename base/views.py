7ae9a5e37eddb37326e578ba43b743fa15a56ec9
leo98ber <leonardoberoes94@gmail.com>
2022-11-02 21:18:42 -0400

    feat: ad generic class viewset to use in all project
    BRWAKING CHANGE: This class allow you to use a basic CRUD without write much code

--- base/views.py
+++ base/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import  viewsets
 
-# Create your views here.
+
+class GeneralListApiView(viewsets.ModelViewSet):
+    serializer_class = None
+
+    def get_queryset(self):
+        model = self.get_serializer().Meta.model
+        return model.objects.filter(state=True)
