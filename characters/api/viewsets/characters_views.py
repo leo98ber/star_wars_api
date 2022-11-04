bc85e966f165a1faa1dd3380c60704cec667c815
leo98ber <leonardoberoes94@gmail.com>
2022-11-04 07:48:21 -0400

    feat: add new method get by pk to use with custom update method

--- characters/api/viewsets/characters_views.py
+++ characters/api/viewsets/characters_views.py
@@ -6,7 +6,6 @@ from rest_framework.response import Response
 
 
 class CharacterViewSet(GeneralViewSet):
-
     """
     This class is the base of the CRUD to create characters and also contains the option to filter
     """
@@ -17,12 +16,29 @@ class CharacterViewSet(GeneralViewSet):
 
     search_fields = ['name', 'performer']
 
+    def get_by_pk(self):
+        """ Make a query by id or primary key """
+        return self.get_serializer().Meta.model.objects.filter(id=self.kwargs['pk'], state=True)
+
     def create(self, request, **kwargs):
         """
-        This method is used to create with a different serializer
+        This method is used to create with a different serializer and send a custom message
         """
         serializer = CharacterSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response({'message': 'Character created successfully!', 'content': serializer.data}, status=status.HTTP_201_CREATED)
+            return Response({'message': 'Character created successfully!', 'content': serializer.data},
+                            status=status.HTTP_201_CREATED)
         return Response({'error': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, pk=None, **kwargs):
+        """
+        This method is used to update with a different serializer and send a custom message
+        """
+        if self.get_by_pk().exists():
+            serializer = CharacterSerializer(instance=self.get_by_pk().get(), data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response({'error': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({'error': 'Object does not exist'}, status=status.HTTP_400_BAD_REQUEST)
